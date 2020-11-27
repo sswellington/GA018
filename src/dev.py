278ace396e6c332494ded8966e2cfc568cf5cbe8
sswellington <wellws@pm.me>
2020-11-27 09:07:49 -0300

    feat: LU decomposition

--- src/dev.py
+++ src/dev.py
@@ -1,4 +1,4 @@
-from sympy import lambdify, diff, hessian, jacobi, cos, sin, exp, pprint
+from sympy import lambdify, diff, hessian, jacobi, cos, sin, exp
 from sympy.matrices import Matrix, eye, zeros, ones, diag, GramSchmidt
 from sympy.plotting import plot, plot3d
 from sympy.abc import x,y
@@ -7,16 +7,25 @@ from Error import *
 from Log import *
 
 ''' Reference
+    https://codetobuy.com/downloads/newton-raphson-optimization-finding-min-or-max/
     https://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html
     http://people.duke.edu/~ccc14/sta-663-2016/12_MultivariateOptimizationAlgorithms.html
     https://sistemas.eel.usp.br/docentes/arquivos/519033/LOM3026/Metodos_numericos_calculo_sistemas_equacoes_nao_lineares.pdf
-
+    https://en.wikipedia.org/wiki/Newton%27s_method_in_optimization
+    
     https://andreask.cs.illinois.edu/cs357-s15/public/demos/12-optimization/Newton's%20Method%20in%20n%20dimensions.html
+    https://www.youtube.com/watch?v=H4rwPpfkPHw
 '''
 
 MAX = 1
 # PATH = 'log/newton/opt/'
 TOLERANCE = 0.00000001 # 10**(-8)
+
+
+def hessiana(function):
+    '''  Hessian: init and set  '''
+    h = hessian(function, (x, y))
+    return (lambdify([x,y], h))
 
 
 def hessian_inverse(function):
@@ -32,7 +41,7 @@ def jacobian_transpose(function):
 
 
 def optimization(f, cx, cy, tol, nmax) :
-    h = hessian_inverse(f)
+    h = hessiana(f)
     j = jacobian_transpose(f)
     
     ''' X0  '''
@@ -40,29 +49,22 @@ def optimization(f, cx, cy, tol, nmax) :
     print('XY =', xy.shape)
     
     for n in range(nmax) : 
-        hessiana = h(float(xy[0]), float(xy[1]))
+        hh = h(float(xy[0]), float(xy[1]))
         jj = j(float(xy[0]), float(xy[1]))
-        root = (xy) - (Matrix(hessiana) * Matrix(jj)) 
-        print('hessiana = ',hessiana.shape )
+        root = (xy) - (Matrix(hh) * Matrix(jj)) 
+        print('hessiana = ',hh.shape )
         print('jacobian = ',jj.shape)
         print('kernel', root) 
         
         print('kernel =', root)
         print(xy[0], xy[1])
-        print('hessiana = ',hessiana)
+        print('hessiana = ',hh)
         print('jacobian = ',jj)
     return (xy)
 
 
 if __name__ == "__main__" :
-    # euler = exp(-( ( ((x-1)**2)/(2*(0.75**2)) ) + ( ((y-2)**2)/(2*(0.5**2)) )  ))
-    # f = 1 - euler + ( 0.04 * (((x-1)**2) + ((y-2)**2)) )
-    
-    # print(euler)
-    
     f = 1 - (exp(-( ( ((x-1)**2)/(2*(0.75**2)) ) + ( ((y-2)**2)/(2*(0.5**2)) )  ))) + ( 0.04 * (((x-1)**2) + ((y-2)**2)) )
-     
-    # f = x**2 * y**3
         
     r = optimization(f, 5.0, 2.0, TOLERANCE, MAX)
     
---
+++ src/matrix-sympy/lu-decomposition.py
@@ -0,0 +1,18 @@
+from sympy.matrices import Matrix, eye, zeros, ones, diag, GramSchmidt
+
+''' LU decomposition
+Returns (L, U, perm) where L is a lower triangular matrix with unit diagonal, 
+U is an upper triangular matrix, 
+and perm is a list of row swap index pairs. 
+
+If A is the original matrix, 
+then A = (L*U).permuteBkwd(perm), and 
+the row permutation matrix P such that P*A = L*U can be computed by P=eye(A.row).
+permuteFwd(perm).
+'''
+
+a = Matrix([[4, 3], [6, 3]])
+L, U, _ = a.LUdecomposition()
+
+print(L)
+print(U)
